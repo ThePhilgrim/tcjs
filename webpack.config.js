3f87d2a911e19d41898755b56e63d6734f335514
ThePhilgrim <76887896+ThePhilgrim@users.noreply.github.com>
2022-12-08 18:06:45 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      title: 'Trans Calc',
+      filename: 'index.html',
+      template: 'src/template.html',
+    }),
+  ],
+};
